28abee976b1ba78953623d85f51d437b4e0f8c54
Z3R0GT <117790541+Z3R0GT@users.noreply.github.com>
2024-01-31 18:03:13 -0500

    1.1.12.23 BASE GAME
    - FIXED CODE
    - RE WRITE
    - USE NEW FUNC

--- main.py
+++ main.py
@@ -7,21 +7,45 @@ from nodes import *
 mapa_principal = Map(120, 29, "#", "Mansión")
 
 pared_1 = Struture(mapa_principal, 1, 1, 116, 20, "&", True)
-coord = [(1, 115, 1),
-         (1, 115, 18)]
 
-for right_y in range(2, 18):
-    coord.append((1, 2, right_y))
-    coord.append((114, 115, right_y))
+pared_1.edit_geometry([(1, 115, 1), (1, 115, 18)])
 
-pared_1.edit_geometry(coord)
+#CUADRADO INTERIOR
+pared_1.create_geometry("Y", 2, 18, 1, 2)
+pared_1.create_geometry("Y", 2, 18, 114, 115)
+
+#CUARTO 1
+pared_1.create_geometry("X", 3, 4, 2, 17)
+pared_1.create_geometry("X", 9, 10, 2, 17)
+
+pared_1.create_geometry("Y", 2, 10, 16, 17)
+
+#ENTRADA
+pared_1.create_geometry("Y", 12, 18, 52, 53)
+pared_1.create_geometry("Y", 12, 18, 67, 68)
+
+pared_1.create_geometry("X", 11, 12, 44, 82)
+pared_1.create_geometry("Y", 4, 12, 43, 44)
+pared_1.create_geometry("Y", 2, 12, 82, 83)
+
+pared_1.create_geometry("X", 4, 5, 44, 82)
+
+#PASADISO 1
+pared_1.create_geometry("X", 4, 5, 17, 44)
+pared_1.create_geometry("X", 9, 10, 17, 44)
+
+#OTROS
+pared_1.create_geometry("Y", 10, 18, 27, 28)
+
+pared_1.create_geometry("X", 8, 9, 83, 115)
+
+
 door = [(59, 19), (60, 19), (59, 18), (60, 18)]
 
-pared_1.set_door(door, "M")
+pared_1.create_door(door, "M")
 
 mapa_principal.add_node(pared_1)
 
-pared_1.get_pre_view()
 #coord = [(4, )]
 
 player = Player(mapa_principal, 40, 25, "X", ..., "...", ["w", "s", "a", "d"], ...)
@@ -32,6 +56,7 @@ mapa_principal.add_node(player)
 #mapa_principal.add_node(cuarto_1)
 
 #mapa_principal.get_pre_view()
+
 start_game(mapa_principal)
 input("> ")
 
