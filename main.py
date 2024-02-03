3493d0351f1125d06c6ead2e2ad37260b05a4453
Z3R0GT <117790541+Z3R0GT@users.noreply.github.com>
2024-02-03 10:53:48 -0500

    1.1.12.23 BASE GAME UPDATE 1.0

--- main.py
+++ main.py
@@ -4,59 +4,62 @@
 
 from nodes import *
 
+const.IS_DEV = False
+
 mapa_principal = Map(120, 29, "#", "Mansión")
 
-pared_1 = Struture(mapa_principal, 1, 1, 116, 20, "&", True)
+mansion = Struture(mapa_principal, 1, 1, 116, 20, "&", True)
 
-pared_1.edit_geometry([(1, 115, 1), (1, 115, 18)])
+mansion.edit_geometry([(1, 115, 1), (1, 115, 18)])
 
 #CUADRADO INTERIOR
-pared_1.create_geometry("Y", 2, 18, 1, 2)
-pared_1.create_geometry("Y", 2, 18, 114, 115)
+mansion.create_geometry("Y", 2, 18, 1, 2)
+mansion.create_geometry("Y", 2, 18, 114, 115)
 
 #CUARTO 1
-pared_1.create_geometry("X", 3, 4, 2, 17)
-pared_1.create_geometry("X", 9, 10, 2, 17)
+mansion.create_geometry("X", 3, 4, 2, 17)
+mansion.create_geometry("X", 9, 10, 2, 17)
 
-pared_1.create_geometry("Y", 2, 10, 16, 17)
+mansion.create_geometry("Y", 2, 10, 16, 17)
 
 #ENTRADA
-pared_1.create_geometry("Y", 12, 18, 52, 53)
-pared_1.create_geometry("Y", 12, 18, 67, 68)
+mansion.create_geometry("Y", 12, 18, 52, 53)
+mansion.create_geometry("Y", 12, 18, 67, 68)
 
-pared_1.create_geometry("X", 11, 12, 44, 82)
-pared_1.create_geometry("Y", 4, 12, 43, 44)
-pared_1.create_geometry("Y", 2, 12, 82, 83)
+mansion.create_geometry("X", 11, 12, 44, 82)
+mansion.create_geometry("Y", 4, 12, 43, 44)
+mansion.create_geometry("Y", 2, 12, 82, 83)
 
-pared_1.create_geometry("X", 4, 5, 44, 82)
+mansion.create_geometry("X", 4, 5, 44, 82)
 
 #PASADISO 1
-pared_1.create_geometry("X", 4, 5, 17, 44)
-pared_1.create_geometry("X", 9, 10, 17, 44)
+mansion.create_geometry("X", 4, 5, 17, 44)
+mansion.create_geometry("X", 9, 10, 17, 44)
 
 #OTROS
-pared_1.create_geometry("Y", 10, 18, 27, 28)
+mansion.create_geometry("Y", 10, 18, 27, 28)
 
-pared_1.create_geometry("X", 8, 9, 83, 115)
+mansion.create_geometry("X", 8, 9, 83, 115)
 
+#PUERTA
+door = [(59, 19), (60, 19), 
+        (59, 18), (60, 18),
+        (59,11), (60, 11),
+        (59, 4), (60, 4),
+        (14, 9), 
+        (28, 17), (40, 9),
+        (9, 9), 
+        (82, 7), (83, 8), (82, 3), 
+        (16, 2)]
 
-door = [(59, 19), (60, 19), (59, 18), (60, 18)]
+mansion.create_door(door, "M")
 
-pared_1.create_door(door, "M")
-
-mapa_principal.add_node(pared_1)
-
-#coord = [(4, )]
+mapa_principal.add_node(mansion)
 
 player = Player(mapa_principal, 40, 25, "X", ..., "...", ["w", "s", "a", "d"], ...)
 mapa_principal.add_node(player)
 
-#cuarto_1 = Struture(mapa_principal, 4, 12,  11, 5, "&", True)
-#cuarto_1.set_door([(2, 0), (3, 0), (4, 0), (7, 1), (7, 2), (7, 3)], "M")
-#mapa_principal.add_node(cuarto_1)
+m = Camera(mapa_principal, player, NMO="a")
 
-#mapa_principal.get_pre_view()
-
-start_game(mapa_principal)
-input("> ")
-
+n = Window(player, mapa_principal, m, "owo")
+n.set_map_n_start()
